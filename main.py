b20d26fff4a5e1d2173ff3d8fc6d89934123b329
Dawid <dawtom97@gmail.com>
2023-07-14 19:30:38 +0200

    first python class

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 
 
-print("test")
+
+
+class Language:
+
+    def __init__(self,name,popularity,year):
+        self.name = name
+        self.popularity = popularity
+        self.year = year
+        # self.__secret()
+    
+    def show_details(self):
+        print("Wybrałeś język " + self.name)
+
+    def __secret(self):
+        print("Sekretne informacje")
+
+
+
+
+javascript = Language("Javascript",90,1993)
+javascript.show_details()
+
+javascript._Language__secret()
+
+php = Language("PHP",70,1991)
+php.show_details()
+
+
+        
